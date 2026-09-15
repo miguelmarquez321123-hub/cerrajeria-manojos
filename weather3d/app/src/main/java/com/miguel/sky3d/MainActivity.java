c76bfbb85bfb14002f98a10e62dbe74726add8dc
miguelmarquez321123-hub <miguelmarquez321123@gmail.com>
2026-09-15 12:28:33 -0400

    Add native WebView activity for live 3D weather

---
+++ weather3d/app/src/main/java/com/miguel/sky3d/MainActivity.java
@@ -0,0 +1,43 @@
+package com.miguel.sky3d;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        webView = new WebView(this);
+        setContentView(webView);
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setAllowFileAccess(true);
+        s.setAllowFileAccessFromFileURLs(true);
+        s.setAllowUniversalAccessFromFileURLs(true);
+        s.setMediaPlaybackRequiresUserGesture(true);
+        s.setCacheMode(WebSettings.LOAD_DEFAULT);
+        s.setBuiltInZoomControls(false);
+        s.setDisplayZoomControls(false);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setBackgroundColor(0xFF0B0716);
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override public void onBackPressed() {
+        if (webView.canGoBack()) webView.goBack(); else super.onBackPressed();
+    }
+}
