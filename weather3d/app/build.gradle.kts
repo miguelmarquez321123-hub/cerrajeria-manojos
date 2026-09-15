7301de51d381feee45cdda4ab161dde1c2e8704d
miguelmarquez321123-hub <miguelmarquez321123@gmail.com>
2026-09-15 12:28:08 -0400

    Add Android app module

---
+++ weather3d/app/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.miguel.sky3d"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.miguel.sky3d"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
